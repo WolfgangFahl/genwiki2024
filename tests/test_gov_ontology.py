55f2f6817d282b37a2999c133840bc26073c3a7e
Wolfgang Fahl <wf@bitplan.com>
2025-11-10 07:31:12 +0100

    upgrades

--- tests/test_gov_ontology.py
+++ tests/test_gov_ontology.py
@@ -1,56 +1,57 @@
 """
 Created on 2025-05-26
 
-@author: wf
+@author: __wf__
 """
 
 from ngwidgets.basetest import Basetest
-from owlready2 import *
-
+from owlapy
 
 class TestGovOntology(Basetest):
     """
-    test GOV Ontology
+    test GOV Ontology using owlapy
     """
 
-    def setUp(self, debug=True, profile=True):
+    def setUp(self, debug: bool = True, profile: bool = True) -> None:
         """
         setUp the test environment
+
+        Args:
+            debug: enable debug output
+            profile: enable profiling
         """
         Basetest.setUp(self, debug=debug, profile=profile)
-        self.onto_url = "http://gov.genealogy.net/ontology.owl"
+        self.onto_url: str = "http://gov.genealogy.net/ontology.owl"
+        self.onto= onto = SyncOntology(self.onto_url)
 
-    def testGovOntology(self):
+
+    def testGovOntology(self) -> None:
         """
         test loading the gov ontology
         """
-        onto = get_ontology(self.onto_url)
-        onto.load()
-        classes = list(
-            default_world.sparql(
-                """
-           SELECT ?x
-           WHERE { ?x a owl:Class . }
-        """
-            )
-        )
-        if self.debug:
-            for (cls,) in classes:
-                print(cls)
-        self.assertTrue(len(classes) > 0)
+        classes = list(self.onto.classes_in_signature())
 
-    def testProperties(self):
+        if self.debug:
+            for owl_class in classes:
+                class_name: str = owl_class.remainder
+                print(class_name)
+
+        classes_count: int = len(classes)
+        self.assertTrue(classes_count > 0)
+
+    def testProperties(self) -> None:
         """
         test loading the gov ontology and showing class properties
         """
+        onto = SyncOntology(self.onto_url)
 
-        def safe_name(entity):
-            return getattr(entity, "name", str(entity))
+        object_props = list(onto.object_properties_in_signature())
+        data_props = list(onto.data_properties_in_signature())
 
-        onto = get_ontology(self.onto_url)
-        onto.load()
         if self.debug:
-            for prop in onto.properties():
-                domain = [safe_name(c) for c in prop.domain]
-                range_ = [safe_name(c) for c in prop.range]
-                print(f"{prop.name}: domain={domain} range={range_}")
+            for prop in object_props:
+                prop_name: str = prop.remainder
+                print(f"ObjectProperty: {prop_name}")
+            for prop in data_props:
+                prop_name: str = prop.remainder
+                print(f"DataProperty: {prop_name}")
